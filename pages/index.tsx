676953595c30ade6ed38236c9317d481aaec2878
mklasinc <miha.klasinc@gmail.com>
2022-07-27 15:47:16 +0200

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Gbg - Cph</title>
+        <meta name="description" content="Description comes here" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Home
