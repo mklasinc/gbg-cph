07eb2779b38333b7ecb21f161b040732ae15e4b2
mklasinc <miha.klasinc@gmail.com>
2022-07-27 16:06:16 +0200

    feat: added basic styles

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
+const path = require('path')
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    prependData: `
+          @import "@/styles/_fonts.scss";
+          @import "@/styles/_variables.scss";
+          @import "@/styles/_functions.scss";
+          @import "@/styles/_animations.scss";
+          @import "@/styles/mixins/_base.scss";
+          @import "@/styles/mixins/_typography.scss";
+          @import '~sass-mq';
+        `,
+  },
+
+  webpack(config) {
+    // glsl support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
+    return config
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfig)
